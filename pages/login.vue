10a52a0c97082dfd169e16f7501de3a5bad36028
cskhw <51100329+cskhw@users.noreply.github.com>
2022-11-28 13:08:47 +0900

    test: test서버 배포 테스트 마무리

--- pages/login.vue
+++ pages/login.vue
@@ -6,7 +6,7 @@
         class="row items-center justify-center q-pa-lg text-weight-bolder font-size-32"
         style="flex: 3; padding-bottom: 48px"
       >
-        오더히어로 검수표 롤백 테스트
+        오더히어로 검수표 롤백 테스트 #2
       </div>
 
       <!-- 사업자 번호 인풋 -->
