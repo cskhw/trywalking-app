c300cbf2f1e6ea9a2c149c3f5791626e2dd3d9b5
cskhw <51100329+cskhw@users.noreply.github.com>
2022-11-29 18:10:52 +0900

    feat: 실서버 시간 테스트

--- pages/login.vue
+++ pages/login.vue
@@ -6,7 +6,7 @@
         class="row items-center justify-center q-pa-lg text-weight-bolder font-size-32"
         style="flex: 3; padding-bottom: 48px"
       >
-        오더히어로 정산관리 11/30 베포 테스트: 캐싱 #2 20221129:17:39
+        오더히어로 정산관리 11/30 베포 테스트: 캐싱 #2 20221129:17:39 {{ date }}
       </div>
 
       <!-- 사업자 번호 인풋 -->
@@ -91,7 +91,7 @@ import axios from "axios";
 const appStore = useAppStore();
 const router = useRouter();
 
-axios.create;
+const date = new Date().toLocaleTimeString();
 
 const isAutoLogin = ref(false);
 const loginForm = reactive({
